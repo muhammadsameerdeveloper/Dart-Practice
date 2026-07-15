ea93224f43feca78e0762017ba2452603b0724b3
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-07-15 15:48:41 +0500

    for & for in & while & do while

---
+++ loop_practice.dart
@@ -0,0 +1,105 @@
+void main() {
+  // for (var i = 1; i <= 20; i++) {
+  //   print(i);
+  // }
+
+  // for (var i = 1; i <= 20; i++) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+  // }
+
+  // for (var i = 10; i >= 1; i--) {
+  //   print(i);
+  // }
+
+  // for (var i = 1; i <= 3; i++) {
+  //   print(i * i * i);
+  // }
+
+  // int sum = 0;
+  // for (var i = 1; i <= 100; i++) {
+  //   sum = i + sum;
+  // }
+  // print(sum);
+
+  // List<String> fruits = ["Apple", "Banana", "Mango", "Orange"];
+  // for (var element in fruits) {
+  //   print(element);
+  // }
+  // int sum = 0;
+  // List<int> numbers = [5, 10, 15, 20, 25];
+  // for (var element in numbers) {
+  //   sum = element + sum;
+  // }
+  // print(sum);
+
+  // List<String> students = ["Ali", "Ahmed", "Usman", "Ayan"];
+  // for (var element in students) {
+  //   print("Welcome $element");
+  // }
+
+  // List<int> marks = [45, 80, 33, 90, 60];
+  // for (var element in marks) {
+  //   if (element >= 40) {
+  //     print("Passing Marks : $element");
+  //   }
+  // }
+
+  // List<int> ages = [12, 18, 25, 15, 30];
+  // for (var element in ages) {
+  //   if (element >= 18) {
+  //     print("Ages : $element");
+  //   }
+  // }
+
+  // int count = 1;
+  // while (count <= 10) {
+  //   print(count++);
+  // }
+
+  // int table = 2;
+  // int count = 1;
+  // while (count <= 10) {
+  //   print("$table x ${count} = ${table * count}");
+  //   count++;
+  // }
+
+  // int count = 20;
+  // while(count>=1){
+  //   print(count--);
+  // }
+
+  // int count = 50;
+  // while (count >= 1) {
+  //   if (count % 2 == 1) {
+  //     print(count);
+  //   }
+  //   count--;
+  // }
+
+  // int sum = 0;
+  // int total = 0;
+  // while (sum <= 20) {
+  //   total = sum + total;
+  //   sum++;
+  // }
+  // print(total);
+
+  // int count = 1;
+  // do {
+  //   print(count++);
+  // } while (count <= 10);
+
+  // int sum = 0;
+  // int table = 5;
+  // do {
+  //   sum++;
+  //   print("$table x $sum = ${table * sum}");
+  // } while (sum <= 9);
+
+  int count = 10;
+  do {
+    print(count--);
+  } while (count >= 1);
+}
