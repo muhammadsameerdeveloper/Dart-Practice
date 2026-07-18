402b994c135fbd17243451af062528205b9629cd
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-07-18 20:08:48 +0500

    list practice

---
+++ list_practice.dart
@@ -0,0 +1,72 @@
+void main() {
+  //   List fruits = ["Orange", "Mango", "Apple", "Banana","Grapes"];
+  //   print(fruits);
+  //   print(fruits[0]);
+  //   print(fruits[4]);
+
+  //   List number = [10, 20, 30, 40, 50];
+  //     number.add(60);
+  //     number.remove(20);
+  //     print(number);
+
+  //   List name = ["Ali", "Ahmed", "Sameer", "Hamza"];
+  //     print(name[2]);
+  //     name.replaceRange(1, 2, ["Ayan"]);
+  //     print(name);
+
+  //   List colors = ["Red", "Green", "Blue", "Black"];
+  //     print("Total Colors ${colors.length}");
+
+  //   List fruits = ["Apple", "Banana", "Orange"];
+  //     if (fruits.contains("Mango") == true) {
+  //     print("Mango is Exist");
+  //     } else {
+  //     print("Mango not found");
+  //   }
+
+  //   List city = [];
+  //     city.addAll(["Karachi","Lahore","Islamabad"]);
+  //     print(city);
+
+  //   List number = [100,200,300];
+  //     number.replaceRange(0,1,[500]);
+  //     print(number);
+
+  //   List subjects = ["Math", "English", "Science"];
+  //     subjects.remove("English");
+  //     print(subjects);
+
+  //   List number = [5, 10, 15, 20];
+  //     number.remove(15);
+  //     print(number);
+
+  //   List fruits = ["Apple", "Banana", "Orange"];
+  //     if(fruits.contains("Banana")){
+  //     print("Banana Found");
+  //     }else{
+  //     print("Banana Not Found");
+  //   }
+
+  //  List alphabet = ["A", "B", "C", "D"];
+  //     alphabet.remove("A");
+  //     alphabet.add("E");
+  //     print(alphabet);
+
+  //   List number1 = [1, 2, 3];
+  //     List number2 = [4, 5, 6];
+  //     number1.addAll(number2);
+  //     print(number1);
+
+  //   List name = ["Ali", "Ahmed", "Sameer", "Hamza"];
+  //     print(name.first);
+  //     print(name.last);
+
+  //   List name = [50, 100, 150];
+  //     name.addAll([200,250]);
+  //     print(name);
+
+  //   List course = ["Flutter", "Dart", "Firebase"];
+  //     course.remove("Firebase");
+  //     course.add("SQL");
+  //     print(course);
+}
