a3adcec40bad69948848de3df12c8bed2a870421
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-07-19 20:30:57 +0500

    map practice

---
+++ map_practice.dart
@@ -0,0 +1,121 @@
+void main() {
+  // Map userInfo = {
+  //   "name":"Sameer",
+  //   "age":"22",
+  //   "city":"Karachi"
+  //   };
+  //   print(userInfo["name"]);
+  //   print(userInfo["city"]);
+
+  // Map userInfo =
+  //   {
+  //   "name": "Ali", "age": 20
+  //   };
+  //   userInfo["age"] = "21";
+  //   print(userInfo);
+
+  // Map userInfo =
+  //   {
+  //   "name": "Ahmed",
+  //   "age": 25
+  //   };
+  //   userInfo["city"]="Lahore";
+  //   print(userInfo);
+
+  // Map userInfo =
+  //   {
+  //   "name": "Hamza",
+  //   "age": 24,
+  //   "city": "Islamabad"
+  //   };
+  //   userInfo.remove("city");
+  //   print(userInfo);
+
+  // Map userInfo = {"name": "Sameer", "course": "Flutter"};
+  //   if (userInfo.containsKey("course") == true) {
+  //   print("course key found");
+  //   }
+  //   if (userInfo.containsValue("dart") == false) {
+  //   print("dart not found");
+  //   }
+
+  // Map userInfo = {
+  //   "name": "Sameer",
+  //   "age": 22,
+  //   "city": "Karachi"
+  //   };
+  //   print(userInfo.keys);
+
+  // Map productDetails = {
+  //   "brand": "Samsung",
+  //   "model": "S25",
+  //   "price": 250000
+  //   };
+  //   print(productDetails.values);
+
+  // Map studentInfo = {
+  //   "name": "Ali",
+  //   "age": 20,
+  //   "city": "Lahore",
+  //   "course": "Flutter"
+  //   };
+  //   print(studentInfo.length);
+
+  // Map userInfo = {
+  //   "name": "Ahmed",
+  //   "age": 25,
+  //   "city": "Karachi"
+  //   };
+  //   userInfo["age"]=26;
+  //   userInfo["city"]="Islamabad";
+  //   print(userInfo);
+
+  // Map userInfo = {
+  //   "name": "Hamza"
+  //   };
+  //   userInfo["age"]=23;
+  //   userInfo["city"]="Peshawar";
+  //   print(userInfo);
+
+  // Map studentInfo = {
+  //   "name": "Sameer",
+  //   "course": "Flutter",
+  //   "city": "Karachi"
+  //   };
+  //   studentInfo.remove("city");
+  //   studentInfo["country"]="Pakistan";
+  //   print(studentInfo);
+
+  // Map studentInfo = {
+  //   "name": "Ali",
+  //   "course": "Dart"
+  //   };
+  //   print(studentInfo.containsKey("age"));
+  //   print(studentInfo.containsValue("Dart"));
+
+  // Map productDetails = {
+  //   "product": "Laptop",
+  //   "price": 80000
+  //   };
+  //   productDetails["price"]=90000;
+  //   print(productDetails);
+
+  // Map userInfo = {
+  //   "name": "Ayan",
+  //   "age": 18,
+  //   "city": "Quetta"
+  //   };
+  //   print(userInfo["name"]);
+  //   print(userInfo["age"]);
+  //   print(userInfo["city"]);
+
+  // Map studentDetails = {
+  //   "name": "Flutter",
+  //   "duration": "3 Months"
+  //   };
+  //   studentDetails["teacher"]="Sameer";
+  //   studentDetails["duration"]="4 Months";
+  //   print(studentDetails.containsKey("fees"));
+  //   print("Keys${studentDetails.keys}     Value${studentDetails.values}");
+  //   print(studentDetails);
+}
