67ba108d789e1034af0810ba91c4b4c808c76d7a
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-07-29 01:16:21 +0500

    on keyword practice

---
+++ on_keyword.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+void main() {
+  // try {
+  //   print("Please Enter Your Age");
+  //   int age = int.parse(stdin.readLineSync()!);
+  //   print(age);
+  // } on FormatException {
+  //   print("Please Enter Numbers Only");
+  // }
+
+  // try {
+  //   List fruits = ["Apple", "Banana", "Mango", "Orange"];
+  //   print("Please Enter Index Number");
+  //   int index = int.parse(stdin.readLineSync()!);
+  //   print(fruits[index]);
+  // } on RangeError {
+  //   print("Invalid Index");
+  // }
+
+  // try {
+  //   print("Enter First Number");
+  //   int num1 = int.parse(stdin.readLineSync()!);
+  //   print("Enter Operator");
+  //   String operator = stdin.readLineSync()!;
+  //   print("Enter Second Number");
+  //   int num2 = int.parse(stdin.readLineSync()!);
+  //   switch (operator) {
+  //     case "/":
+  //       print(num1 ~/ num2);
+  //       break;
+  //     default:
+  //       print("Invalid Operator");
+  //   }
+  // } catch (e) {
+  //   print("Cannot Divide By Zero");
+  // }
+
+  // try {
+  //   print("Please Enter Day");
+  //   String userInput = stdin.readLineSync()!;
+  //   Days day = Days.values.byName(userInput);
+  //   switch (day) {
+  //     case Days.monday:
+  //       print("${day.name} -> Start of Week");
+  //       break;
+  //     case Days.friday:
+  //       print("${day.name} -> Weekend is Near");
+  //       break;
+  //     case Days.sunday:
+  //       print("${day.name} -> Holiday");
+  //       break;
+  //     default:
+  //       print("Invalid Input");
+  //   }
+  // } on ArgumentError {
+  //   print("Invalid Day");
+  // }
+  //   try {
+  //     List marks = [80, 70, 90];
+  //     print("Please Enter Index");
+  //     int index = int.parse(stdin.readLineSync()!);
+  //     print("Please Enter Number");
+  //     int number = int.parse(stdin.readLineSync()!);
+  //     print("Index Output = ${marks[index]}");
+  //     print("Number Output = ${100 ~/ number}");
+  //   } on RangeError {
+  //     print("Invalid Index");
+  //   } on IntegerDivisionByZeroException {
+  //     print("Cannot Devide By Zero");
+  //   }
+}
+
+// enum Days { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
