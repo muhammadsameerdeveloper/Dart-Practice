340432145d735c38bf51ec500db3d4b9867cfde6
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-07-30 16:55:32 +0500

    finally block practice

---
+++ finally_block.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+void main() {
+  // try {
+  //   print("Please Enter Number");
+  //   int num1 = int.parse(stdin.readLineSync()!);
+  //   print(num1);
+  // } on FormatException {
+  //   print("Numbers Only");
+  // } finally {
+  //   print("Program Finished");
+  // }
+
+  // List fruits = ["Apple", "Banana", "Mango"];
+  // try {
+  //   print("Please Enter Index");
+  //   int index = int.parse(stdin.readLineSync()!);
+  //   print(fruits[index]);
+  // } catch (e) {
+  //   print(e);
+  // } finally {
+  //   print("Task Completed");
+  // }
+
+  // try {
+  //   print("Please Enter First Number");
+  //   int num1 = int.parse(stdin.readLineSync()!);
+  //   print("Please Enter Operator");
+  //   String operation = stdin.readLineSync()!;
+  //   print("Please Enter Second Number");
+  //   int num2 = int.parse(stdin.readLineSync()!);
+  //   switch (operation) {
+  //     case "+":
+  //       print(num1 + num2);
+  //       break;
+  //     case "*":
+  //       print(num1 * num2);
+  //       break;
+  //     case "-":
+  //       print(num1 - num2);
+  //       break;
+  //     case "/":
+  //       print(num1 ~/ num2);
+  //       break;
+  //     default:
+  //       print("Invalid");
+  //   }
+  // } catch (e) {
+  //   print(e);
+  // } finally {
+  //   print("Thanks for using Calculator");
+  // }
+
+  // try {
+  //   print("Enter Number");
+  //   int num1 = int.parse(stdin.readLineSync()!);
+  //   print(num1);
+  // } catch (e) {
+  //   print(e);
+  // } finally {
+  //   print("End of Program");
+  // }
+
+  try {
+    int num1 = 5;
+    print(num1);
+  } catch (e) {
+    print(e);
+  } finally {
+    print("Program End");
+  }
+}
