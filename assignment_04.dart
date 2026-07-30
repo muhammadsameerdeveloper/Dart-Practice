1fe5ead97077706c7dba963fe5fa82384d8b5f4f
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-07-31 00:41:44 +0500

    assignment 04

--- assignment_04.dart
+++ assignment_04.dart
@@ -19,13 +19,13 @@ void main() {
   //   if (Student[i]["marks"] >= 90) {
   //     print("Grade : A+");
   //     passTotal++;
-  //   } else if (Student[i]["marks"] >= 80 || Student[i]["marks"] <= 89) {
+  //   } else if (Student[i]["marks"] >= 80 && Student[i]["marks"] <= 89) {
   //     print("Grade : A");
   //     passTotal++;
-  //   } else if (Student[i]["marks"] >= 70 || Student[i]["marks"] <= 79) {
+  //   } else if (Student[i]["marks"] >= 70 && Student[i]["marks"] <= 79) {
   //     print("Grade : B");
   //     passTotal++;
-  //   } else if (Student[i]["marks"] >= 60 || Student[i]["marks"] <= 69) {
+  //   } else if (Student[i]["marks"] >= 60 && Student[i]["marks"] <= 69) {
   //     print("Grade : C");
   //     passTotal++;
   //   } else {
@@ -109,4 +109,78 @@ void main() {
   // print("Ahmed : $ahmedVote");
   // print("Sara : $saraVote");
   // print("Hamza : $hamzaVote");
+
+  // Question 4: Employee Salary Report
+
+  // List employees = [
+  //   {"name": "Ali", "salary": 45000, "department": "IT"},
+  //   {"name": "Ahmed", "salary": 40000, "department": "HR"},
+  //   {"name": "Sara", "salary": 55000, "department": "Accounts"},
+  //   {"name": "Ayesha", "salary": 60000, "department": "IT"},
+  //   {"name": "Usman", "salary": 38000, "department": "HR"},
+  //   {"name": "Fatima", "salary": 50000, "department": "Accounts"},
+  // ];
+  // int departIT = 0;
+  // int departHR = 0;
+  // int departAccount = 0;
+  // int totalSalary = 0;
+  // for (var i = 0; i < employees.length; i++) {
+  //   if (employees[i]["salary"] > 50000) {
+  //     print(
+  //       "Name : ${employees[i]["name"]} , Salary : ${employees[i]["salary"]} , Department : ${employees[i]["department"]}",
+  //     );
+  //     if (employees[i]["department"] == "IT") {
+  //       departIT++;
+  //     } else if (employees[i]["department"] == "HR") {
+  //       departHR++;
+  //     } else if (employees[i]["department"] == "Accounts") {
+  //       departAccount++;
+  //     }
+  //     totalSalary = employees[i]["salary"] + totalSalary;
+  //   }
+  // }
+  // print("IT Department Total Employees : $departIT");
+  // print("HR Department Total Employees : $departHR");
+  // print("Account Department Total Employees : $departAccount");
+  // print(
+  //   "Average Salary All 50K Upper Per Employees : ${totalSalary / (departIT + departHR + departAccount)}",
+  // );
+
+  // Question 5: Cricket Scoreboard
+
+  // print("=====================Cricket Scoreboard=====================");
+
+  // List players = [
+  //   {"name": "Babar", "runs": 75},
+  //   {"name": "Rizwan", "runs": 62},
+  //   {"name": "Fakhar", "runs": 48},
+  //   {"name": "Shaheen", "runs": 15},
+  //   {"name": "Naseem", "runs": 10},
+  //   {"name": "Haris", "runs": 55},
+  //   {"name": "Shadab", "runs": 38},
+  // ];
+  // int totalRuns = 0;
+  // int fiftyPlus = 0;
+  // List fiftyPlusName = [];
+  // var highestScorer = players.reduce((a, b) => a["runs"] > b["runs"] ? a : b);
+  // var lowestScorer = players.reduce((a, b) => a["runs"] < b["runs"] ? a : b);
+  // for (var i = 0; i < players.length; i++) {
+  //   print("Player Name : ${players[i]["name"]} , Runs : ${players[i]["runs"]}");
+  //   totalRuns = players[i]["runs"] + totalRuns;
+  //   if (players[i]["runs"] >= 50) {
+  //     fiftyPlusName.add(players[i]);
+  //     fiftyPlus++;
+  //   }
+  // }
+  // print("Total Runs : $totalRuns");
+  // print("Highest Scorer : $highestScorer");
+  // print("Lowest Scorer : $lowestScorer");
+  // print(
+  //   "Total Player 50+ Runs : Number Of Player $fiftyPlus \nPlayer Name : ${fiftyPlusName.map((fiftyPlusName) => "${fiftyPlusName["name"]} : ${fiftyPlusName["runs"]}").toList()}",
+  // );
+  // if (totalRuns > 300) {
+  //   print("Result : Team Won");
+  // } else {
+  //   print("Result : Team Lost");
+  // }
 }
