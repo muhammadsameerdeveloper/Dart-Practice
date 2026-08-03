7d223417836f57624de43e304f29a908585d9a6c
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-08-04 00:51:51 +0500

    Override Super Practice

--- OOPs/override_super.dart
+++ OOPs/override_super.dart
@@ -16,20 +16,137 @@
 //   }
 // }
 
-void main() {
-  Car obj = Car();
-  obj.start();
-}
+// void main() {
+//   Car obj = Car();
+//   obj.start();
+// }
 
-class Vehicle {
-  void start() {
-    print("Vehicle Start");
-  }
-}
+// class Vehicle {
+//   void start() {
+//     print("Vehicle Start");
+//   }
+// }
 
-class Car extends Vehicle {
-  @override
-  void start() {
-    print("Car Start");
-  }
-}
+// class Car extends Vehicle {
+//   @override
+//   void start() {
+//     print("Car Start");
+//   }
+// }
+
+// void main() {
+//   Doctor obj1 = Doctor();
+//   obj1.work();
+//   Teacher obj2 = Teacher();
+//   obj2.work();
+// }
+
+// class Person {
+//   void work() {
+//     print("Person Working");
+//   }
+// }
+
+// class Doctor extends Person {
+//   @override
+//   void work() {
+//     print("Doctor Working");
+//   }
+// }
+
+// class Teacher extends Person {
+//   @override
+//   void work() {
+//     print("Teacher Working");
+//   }
+// }
+// void main() {
+//   Circle obj1 = Circle();
+//   obj1.draw();
+//   Rectangle obj2 = Rectangle();
+//   obj2.draw();
+// }
+
+// class Shape {
+//   draw() {
+//     print("Drawing");
+//   }
+// }
+
+// class Circle extends Shape {
+//   @override
+//   draw() {
+//     print("Circle Drawing");
+//   }
+// }
+
+// class Rectangle extends Shape {
+//   @override
+//   draw() {
+//     print("Rectangle Drawing");
+//   }
+// }
+// void main() {
+//   Eagle obj = Eagle();
+//   obj.fly();
+// }
+
+// class Bird {
+//   fly() {
+//     print("Bird Flying");
+//   }
+// }
+
+// class Eagle extends Bird {
+//   @override
+//   fly() {
+//     print("Eagle Flies High");
+//   }
+// }
+
+// print("=====================Super Start=====================");
+
+// void main() {
+//   Dog obj = Dog();
+//   obj.eat();
+// }
+
+// class Animal {
+//   void eat() {
+//     print("Animal Eat");
+//   }
+// }
+
+// class Dog extends Animal {
+//   @override
+//   void eat() {
+//     super.eat();
+//     print("Dog eats meat");
+//   }
+// }
+// void main() {
+//   Cat obj = Cat();
+// }
+
+// class Animal {
+//   Animal(String name) {
+//     print(name);
+//   }
+// }
+
+// class Cat extends Animal {
+//   Cat() : super("Kitty");
+// }
+
+// void main() {
+//   panter obj = panter();
+//   print(obj.color);
+// }
+
+// class Colors {
+//   String color = "Black";
+// }
+
+// class panter extends Colors {
+//   panter() : super();
+// }
