6d4ecd96be4068ffabbc2bac7443ee9c6870cad8
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-08-05 12:00:50 +0500

    Abstraction Practice

--- OOPs/abstraction.dart
+++ OOPs/abstraction.dart
@@ -1,15 +1,85 @@
-void main() {
-  Car obj = Car();
-  obj.startEngine();
-}
+// void main() {
+//   Car obj = Car();
+//   obj.startEngine();
+// }
 
-abstract class Vehicle {
-  startEngine();
-}
+// abstract class Vehicle {
+//   startEngine();
+// }
 
-class Car extends Vehicle {
-  @override
-  startEngine() {
-    print("Car Engine Started");
-  }
-}
+// class Car extends Vehicle {
+//   @override
+//   startEngine() {
+//     print("Car Engine Started");
+//   }
+// }
+
+// void main() {
+//   Developer obj = Developer();
+//   obj.work();
+// }
+
+// abstract class Employee {
+//   work();
+// }
+
+// class Developer extends Employee {
+//   @override
+//   void work() {
+//     print("Cording");
+//   }
+// }
+
+// void main() {
+//   Parrot obj = Parrot();
+//   obj.eat();
+//   obj.fly();
+// }
+
+// abstract class Bird {
+//   eat() {
+//     print("Bird Eating");
+//   }
+
+//   fly();
+// }
+
+// class Parrot extends Bird {
+//   @override
+//   fly() {
+//     print("Parrot is Flying");
+//   }
+// }
+
+// void main() {
+//   Cat obj1 = Cat();
+//   Dog obj2 = Dog();
+//   obj1.sound();
+//   obj2.sound();
+// }
+
+// abstract class Animal {
+//   eat() {
+//     print("Animal Eating");
+//   }
+
+//   sleep() {
+//     print("Animal Sleep");
+//   }
+
+//   sound();
+// }
+
+// class Cat extends Animal {
+//   @override
+//   sound() {
+//     print("Maou");
+//   }
+// }
+
+// class Dog extends Animal {
+//   @override
+//   sound() {
+//     print("Bhooo");
+//   }
+// }
