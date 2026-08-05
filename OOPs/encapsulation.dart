ea9248fe200e454e7d2d1acadca2f44f5c06ac5b
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-08-05 18:28:34 +0500

    Encapsulation Practice

--- OOPs/encapsulation.dart
+++ OOPs/encapsulation.dart
@@ -2,8 +2,8 @@
 //   Student obj = Student();
 //   obj._name = "Sameer";
 //   obj._age = 25;
-//   print(obj._name);
-//   print(obj._age);
+//   print(obj.name);
+//   print(obj.age);
 // }
 
 // class Student {
@@ -30,8 +30,8 @@
 //   Student obj = Student();
 //   obj.name = "Sameer";
 //   obj.marks = 1;
-//   print(obj._name);
-//   print(obj._marks);
+//   print(obj.name);
+//   print(obj.marks);
 // }
 
 // class Student {
@@ -56,5 +56,94 @@
 
 //   int get marks {
 //     return _marks;
+//   }
+// }
+// void main() {
+//   BankAccount obj = BankAccount();
+//   obj.balance = 5000;
+//   obj.balance = -200;
+//   print(obj.balance);
+// }
+
+// class BankAccount {
+//   int _balance = 0;
+//   set balance(int balance) {
+//     if (balance > 0) {
+//       _balance = balance;
+//     } else {
+//       print("Invalid Amount");
+//     }
+//   }
+
+//   int get balance {
+//     return _balance;
+//   }
+// }
+
+// void main() {
+//   Product obj = Product();
+//   obj._name = "Laptop";
+//   obj._price = 85000;
+//   print(obj.name);
+//   print(obj.price);
+// }
+
+// class Product {
+//   String _name = "";
+//   int _price = 0;
+
+//   set name(String name) {
+//     _name = name;
+//   }
+
+//   set price(int price) {
+//     if (price > 0) {
+//       _price = price;
+//     } else {
+//       print("Invalid Price");
+//     }
+//   }
+
+//   String get name {
+//     return _name;
+//   }
+
+//   int get price {
+//     return _price;
+//   }
+// }
+
+// void main() {
+//   Employee obj = Employee();
+
+//   obj.name = "Sameer";
+//   obj.salary = 5000;
+
+//   print(obj.name);
+//   print(obj.salary);
+// }
+
+// class Employee {
+//   String _name = "";
+//   int _salary = 0;
+
+//   set name(String name) {
+//     _name = name;
+//   }
+
+//   set salary(int salary) {
+//     if (salary >= 10000) {
+//       _salary = salary;
+//     } else {
+//       print("Minimum Salary is 10000");
+//     }
+//   }
+
+//   String get name {
+//     return _name;
+//   }
+
+//   int get salary {
+//     return _salary;
 //   }
 // }
