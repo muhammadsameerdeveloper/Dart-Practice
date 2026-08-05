e9913442cc71721c341fe2254a1bd74f4e263b3c
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-08-05 17:09:11 +0500

    Encapsulation Practice

---
+++ OOPs/encapsulation.dart
@@ -0,0 +1,60 @@
+// void main() {
+//   Student obj = Student();
+//   obj._name = "Sameer";
+//   obj._age = 25;
+//   print(obj._name);
+//   print(obj._age);
+// }
+
+// class Student {
+//   String _name = "";
+//   int _age = 0;
+
+//   set name(String name) {
+//     _name = name;
+//   }
+
+//   set age(int age) {
+//     _age = age;
+//   }
+
+//   String get name {
+//     return _name;
+//   }
+
+//   int get age {
+//     return _age;
+//   }
+// }
+// void main() {
+//   Student obj = Student();
+//   obj.name = "Sameer";
+//   obj.marks = 1;
+//   print(obj._name);
+//   print(obj._marks);
+// }
+
+// class Student {
+//   String _name = "";
+//   int _marks = 0;
+
+//   set name(String name) {
+//     _name = name;
+//   }
+
+//   set marks(int marks) {
+//     if (marks > 0 && marks <= 100) {
+//       _marks = marks;
+//     } else {
+//       print("Invalid Marks");
+//     }
+//   }
+
+//   String get name {
+//     return _name;
+//   }
+
+//   int get marks {
+//     return _marks;
+//   }
+// }
