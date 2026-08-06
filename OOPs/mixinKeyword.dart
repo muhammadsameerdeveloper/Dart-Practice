38f721272b3eb9e08521d4af00b5a1ea6d150058
muhammadsameerdeveloper <muhammadsameerdeveloper@gmail.com>
2026-08-06 14:09:27 +0500

    Mixin Keyword

---
+++ OOPs/mixinKeyword.dart
@@ -0,0 +1,70 @@
+// void main() {
+//   Human obj = Human();
+//   obj.walk();
+// }
+
+// mixin Walk {
+//   walk() {
+//     print("Walking...");
+//   }
+// }
+
+// class Human with Walk {}
+
+// void main() {
+//   Cat obj1 = Cat();
+//   Dog obj2 = Dog();
+//   obj1.eat();
+//   obj2.eat();
+// }
+
+// mixin Eat {
+//   eat() {
+//     print("Eating...");
+//   }
+// }
+
+// class Cat with Eat {}
+
+// class Dog with Eat {}
+
+// void main() {
+//   Duck obj = Duck();
+//   obj.fly();
+//   obj.swim();
+// }
+
+// mixin Fly {
+//   fly() {
+//     print("Flying...");
+//   }
+// }
+// mixin Swim {
+//   swim() {
+//     print("Swimming...");
+//   }
+// }
+
+// class Duck with Fly, Swim {}
+
+import 'dart:async';
+
+void main() {
+  Horse obj = Horse();
+  obj.eat();
+  obj.run();
+}
+
+class Animal {
+  eat() {
+    print("Eating...");
+  }
+}
+
+mixin Run {
+  run() {
+    print("Running...");
+  }
+}
+
+class Horse extends Animal with Run {}
